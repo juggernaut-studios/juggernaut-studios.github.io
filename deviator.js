19d62008703f6bba68e958dadaebe8f7819e91cd
unknown <sandeep_bamane@PS7929.persistent.co.in>
2014-07-14 17:52:30 +0530

    added json p

---
+++ deviator.js
@@ -0,0 +1,30 @@
+
+var parentContainer= "mainWrapper", gDeviator, zhampu = zhampu || {};
+
+zhampu.WrapperClass = function()
+{
+    "use strict";
+    this.drawMenu();
+};
+
+zhampu.WrapperClass.prototype = {
+    drawMenu :function()
+    {
+              
+    }
+};
+
+zhampu.dataStore ={
+    "JS-Prototype" :"<p>Just think of constructor functions as classes and the properties of the prototype (ie of the object referenced by the constructor function's prototype property) as shared members, ie members which are the same for each instance. In class-based systems, methods are implemented the same way for each instance, so methods are normally added to the prototype, whereas an object's fields are instance-specific and therefore added to the object itself during construction.</p> <p>any object-oriented code you write in JavaScript is a paradigm of prototypal inheritance. JavaScript simply doesn't have classical inheritance</p>"
+    
+    
+};
+
+wndow.onload = function() {
+    "use strict";
+    zhampu.WrapperClass();
+};
+
+ 
+
+
